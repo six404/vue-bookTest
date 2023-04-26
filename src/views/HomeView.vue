087ad590755de1478b711a2a86e64810790990f9
鹏程 李 <1494205676@qq.com>
2023-04-26 22:05:33 +0800

    hello

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
  * @Author: 鹏程 李 1494205676@qq.com
  * @Date: 2023-04-20 22:59:23
  * @LastEditors: 鹏程 李 1494205676@qq.com
- * @LastEditTime: 2023-04-25 22:14:42
+ * @LastEditTime: 2023-04-26 22:05:32
  * @FilePath: \book-front\src\views\HomeView.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -144,23 +144,23 @@
             console.log("调用del接口信息：",resp.data);
             const{code} = resp.data
             if(code === '00000'){
-            this.$message({
-              message: '删除成功！',
-              type: 'success',
-              duration : 1500,
-              onClose:()=>{
-                this.getDataList()
-              }
-            });
-          }else{
-            this.$message({
-              message:'删除失败！',
-              duration:1500,
-              onClose:()=>{
-                this.getDataList()
-              }
-            })
-          }
+              this.$message({
+                message: '删除成功！',
+                type: 'success',
+                duration : 1500,
+                onClose:()=>{
+                  this.getDataList()
+                }
+              });
+            }else{
+              this.$message({
+                message:'删除失败！',
+                duration:1500,
+                onClose:()=>{
+                  this.getDataList()
+                }
+              })
+            }
           })
         }).catch((error)=>{
           console.log(error);
